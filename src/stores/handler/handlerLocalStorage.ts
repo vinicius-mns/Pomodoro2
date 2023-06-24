cfba914287751555c4c165e3c1655356f00a53db
vinicius silva <vinicius_dev@hotmail.com>
2023-06-24 18:59:50 -0300

    feat(src/stores/handler)[state]: Handler createLocalStorage
    Cria handler create criar, caso nao exista, um novo valor no localStorage

--- src/stores/handler/handlerLocalStorage.ts
+++ src/stores/handler/handlerLocalStorage.ts
@@ -1,4 +1,10 @@
 class HandlerLocalStorage {
+  createIfNotExist(key: string, value: object | undefined) {
+
+    if(!localStorage.getItem(key)) {
+      localStorage.setItem(key, JSON.stringify(value))
+    }
+  }
 }
 
 export default HandlerLocalStorage
