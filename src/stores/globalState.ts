aa9c8bf2345b61a0c5fc4b21762ea6c00845c688
vinicius silva <vinicius_dev@hotmail.com>
2023-07-08 18:28:21 -0300

    feat(src/stores/)[state]: Importar handlers nos stores

--- src/stores/globalState.ts
+++ src/stores/globalState.ts
@@ -2,6 +2,9 @@ import { defineStore } from "pinia"
 import { reactive } from "vue"
 import { type IGlobalState } from "@/interfaces"
 
+import { HandleDate, HandleStorage } from "@/utils"
+const handleDate = new HandleDate()
+
 const globalStateName = 'globalState'
 export default defineStore(globalStateName, () => {
 
