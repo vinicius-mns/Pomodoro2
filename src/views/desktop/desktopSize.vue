f95ca731fcff1c14f64f8bf66b71d309d53cab8e
vinicius silva <vinicius_dev@hotmail.com>
2023-07-08 19:01:36 -0300

    feat(src/views/desktop/)[desktop]: Criar view para desktop
    Pagina que renderizara quando o tamanho da tela for para desktop

---
+++ src/views/desktop/desktopSize.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main class="desktop">
+  </main>
+</template>
+
+<style scoped lang="scss">
+// desktop
+@media screen and (min-width: 1024px) {
+  .desktop {
+    display: flex;
+  }
+}
+</style>
