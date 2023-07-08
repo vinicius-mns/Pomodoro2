05fa3030a6fabd51dedc2775a3af76a81153e67d
vinicius silva <vinicius_dev@hotmail.com>
2023-07-08 18:47:48 -0300

    feat(src/stores)[state]: Criar estado global inicial
    Cria estado global inicial usando os handles, utiliza o handle para permanencia de estado no local storage

--- src/stores/globalState.ts
+++ src/stores/globalState.ts
@@ -8,7 +8,13 @@ const handleDate = new HandleDate()
 const globalStateName = 'globalState'
 export default defineStore(globalStateName, () => {
 
-  const state: IGlobalState[] = reactive({})
+  const initState = (): IGlobalState[] => {
+    return handleDate.daysOfWeek.map((day) => ({folderName: day, folder: [{name: day, time: 100}]}))
+  }
+
+  const handleStorage = new HandleStorage(globalStateName, initState())
+
+  const state: IGlobalState[] = reactive(handleStorage.read as IGlobalState[])
 
   return {
     state
