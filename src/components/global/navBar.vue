0e9ebc17d2a49bc7f0258eaafd58db88a6d7d52c
vinicius silva <vinicius_dev@hotmail.com>
2023-07-08 03:02:13 -0300

    feat(src/components/global/)[desktop]: Criar barra de navegacao (rotas)
    Cria barra lateral, componente para nevegacao pelas rotas

---
+++ src/components/global/navBar.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import { useGlobalState } from '@/stores/globalState';
+import { ref } from 'vue';
+
+const globalState = useGlobalState()
+
+const paths = ref(globalState.getAllpaths())
+</script>
+
+<template>
+  <aside>
+    <nav v-for="(day, i) of paths" :key="i">
+      <RouterLink :to="'/' + day">{{ day }}</RouterLink>
+    </nav>
+  </aside>
+</template>
+
+<style scoped lang="scss">
+// desktop
+@media screen and (min-width: 1024px) {
+  aside {
+    height: 100vh;
+    width: 10%;
+    background-color: rgb(45, 45, 45);
+  }
+}
+</style>
