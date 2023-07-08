cf5e30c47858507501093970f4effdec2c02231f
vinicius silva <vinicius_dev@hotmail.com>
2023-07-07 23:00:04 -0300

    feat(src/util/)[utils]: Cria motodo getToday
    Metodo retorna o dia da semana

--- src/utils/dates.ts
+++ src/utils/dates.ts
@@ -2,4 +2,9 @@ export default class HandleDate {
   constructor(
     private _Dayslist = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sabado']
   ){}
+
+  public get toDay() {
+    const dayNumber = new Date().getDay()
+    return this._Dayslist[dayNumber]
+  }
 }
