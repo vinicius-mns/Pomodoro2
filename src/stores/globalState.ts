e0e98532d820cca13b827a4e34d902559ece9371
vinicius silva <vinicius_dev@hotmail.com>
2023-07-08 18:08:34 -0300

    feat(src/stores/)[state]: Implementar interface IGlobalState
    utiliza interface no estado global

--- src/stores/globalState.ts
+++ src/stores/globalState.ts
@@ -1,10 +1,11 @@
 import { defineStore } from "pinia"
 import { reactive } from "vue"
+import { type IGlobalState } from "@/interfaces"
 
 const globalStateName = 'globalState'
 export default defineStore(globalStateName, () => {
 
-  const state = reactive({})
+  const state: IGlobalState[] = reactive({})
 
   return {
     state
