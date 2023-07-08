808b215179e6278d8bdb108cc0f724951a108306
vinicius silva <vinicius_dev@hotmail.com>
2023-07-08 16:56:00 -0300

    feat(src/components/global/desktop/)[desktop]: Criar sideBar
    Cria barra lateral, para conter: barra de navegacao, ajustes, botao para abrir configuracoes,

---
+++ src/components/global/desktop/sideBar.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <aside>
+  </aside>
+</template>
+
+<style scoped lang="scss">
+// desktop
+@media screen and (min-width: 1024px) {
+  aside {
+    height: 100vh;
+    width: 16%;
+    display: flex;
+    flex-direction: column;
+    background-color: rgb(45, 45, 45);
+  }
+}
+</style>
