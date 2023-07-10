6475de68475ef819978a6defcf6a17fc2e3ab17c
vinicius silva <vinicius_dev@hotmail.com>
2023-07-10 03:25:31 -0300

    feat(src/components/desktop/)[desktop]: Adicionar css sideBar
    Adiciona estilo a barra lateral

--- src/components/global/desktop/sideBar.vue
+++ src/components/global/desktop/sideBar.vue
@@ -11,12 +11,9 @@ import navBar from './navBar.vue';
 <style scoped lang="scss">
 // desktop
 @media screen and (min-width: 1024px) {
-  aside {
-    height: 100vh;
-    width: 16%;
-    display: flex;
-    flex-direction: column;
-    background-color: rgb(45, 45, 45);
-  }
+aside {
+  width: 26%;
+  height: 100vh;
+  background-color: red;
 }
 </style>
