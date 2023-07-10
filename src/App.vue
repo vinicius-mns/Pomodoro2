76d287d5f303ffe5fb00fe4cf0a1c95356436285
vinicius silva <vinicius_dev@hotmail.com>
2023-07-10 02:14:01 -0300

    refactor(src/app)[desktop, tablet, mobile]: Definir tamanho de tela
    Define no app as 3 formas de renderizacao, desktop, tablet, mobile

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,24 @@
 <script setup lang="ts">
-import { RouterView } from 'vue-router';
-import navBar from './components/navBar/navBar.vue'
+import desktopSizeVue from './views/desktop/desktopSize.vue';
+import { ref } from 'vue';
+
+const divice = ref('desktop')
+
+window.addEventListener('resize', () => {
+  const size = window.innerWidth
+  if(size >= 1024) divice.value = 'desktop'
+  if(size > 767 && size < 1023) divice.value = 'tablet'
+  if(size< 767) divice.value = 'mobile'
+})
 </script>
 
 <template>
   <div class="home-page">
-    <navBar />
-    <RouterView />
+    <desktopSizeVue v-if="divice === 'desktop'" />
+    <div  v-if="divice === 'tablet'"><h1>Tablet em contrucao</h1></div>
+    <div  v-if="divice === 'mobile'"><h1>mobile em contrucao</h1></div>
   </div>
 </template>
 
 <style scoped lang="scss">
-// desktop
-@media screen and (min-width: 1024px) {
-  .home-page {
-    display: flex;
-  }
-}
 </style>
